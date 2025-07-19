a877ff6eeb6ac562da9aa9bdaa52f4f195afc9e7
Savo Shen <savo_shen@qq.com>
2025-07-19 13:45:11 +0800

    修改test

--- ruoyi-admin-wms/src/test/java/com/ruoyi/test/AssertUnitTest.java
+++ ruoyi-admin-wms/src/test/java/com/ruoyi/test/AssertUnitTest.java
@@ -16,30 +16,30 @@ public class AssertUnitTest {
     @Test
     public void testAssertEquals() {
         Assertions.assertEquals("666", new String("666"));
-        Assertions.assertNotEquals("666", new String("666"));
+        Assertions.assertNotEquals("666", new String("777"));
     }
 
-    @DisplayName("测试 assertSame 方法")
-    @Test
-    public void testAssertSame() {
-        Object obj = new Object();
-        Object obj1 = obj;
-        Assertions.assertSame(obj, obj1);
-        Assertions.assertNotSame(obj, obj1);
-    }
+    // @DisplayName("测试 assertSame 方法")
+    // @Test
+    // public void testAssertSame() {
+    //     Object obj = new Object();
+    //     Object obj1 = obj;
+    //     Assertions.assertSame(obj, obj1);
+    //     Assertions.assertNotSame(obj, obj1);
+    // }
 
-    @DisplayName("测试 assertTrue 方法")
-    @Test
-    public void testAssertTrue() {
-        Assertions.assertTrue(true);
-        Assertions.assertFalse(true);
-    }
+    // @DisplayName("测试 assertTrue 方法")
+    // @Test
+    // public void testAssertTrue() {
+    //     Assertions.assertTrue(true);
+    //     Assertions.assertFalse(true);
+    // }
 
-    @DisplayName("测试 assertNull 方法")
-    @Test
-    public void testAssertNull() {
-        Assertions.assertNull(null);
-        Assertions.assertNotNull(null);
-    }
+    // @DisplayName("测试 assertNull 方法")
+    // @Test
+    // public void testAssertNull() {
+    //     Assertions.assertNull(null);
+    //     Assertions.assertNotNull(null);
+    // }
 
 }
